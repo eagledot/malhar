dd4fd9a352b238edeca6cf72b476fb07dba4431b
eagledot <usernainn@zohomail.in>
2024-10-04 14:34:38 +0530

    better,clear

--- test/search_filenames.py
+++ test/search_filenames.py
@@ -1,12 +1,13 @@
 # A simple programme to showcase usage.
 # It indexes all the immediate filenames returned by `os.listdir(...)` and then run some queries.
 
+# Run: python search_filename.py
 import sys
 import os
-sys.path.append("D://nim_learning")
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..",".."))
 from malhar import FuzzyIndex
 
-filenames = os.listdir("D://nim_learning")
+filenames = os.listdir("C://users/random/downloads")
 
 # index
 db = FuzzyIndex(name = "filesearch")
@@ -23,28 +24,5 @@ def show_results(query:str):
     for pkey,score in pkey_scores[:top_k]:
         print(filenames[pkey], score)
 
-query = "avbin" # expected badminton (fOR MY contents)
-query = "wjSgoWPm5bWAhXB"
-print(db._tokenize(query))
-sample_test = "(http://freebsdfoundation.blogspot.com/2014/11/freeBSD-foundation-announces-generous.html?HN)"
-print(db._tokenize(sample_test))
-print(db._tokenize("freebsd"))
-print(db._tokenize("freebsdfoundation"))
+show_results(query = "wulvrine")
 
-db._debug_new(query = "freebsd", paragraph = sample_test)
-
-db._debug_new(query = query, paragraph = "wjsogpmwb5awh")
-# db._debug_new(query = "tnnise", paragraph = "tennis")
-
-# we would want to compare some results....
-
-
-# show_results(query)
-
-
-
-
-# print(db._tokenize(query))
-# print(db._tokenize("dininghall"))
-# print(db._tokenize("bean bags are nice!"))
-# db._debug_new(query = query, paragraph = "bean bags are nice!")
