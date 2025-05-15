073101718cd6d4bcc23af993629be6f4a74c7579
eagledot <usernainn@zohomail.in>
2025-05-15 12:30:46 +0530

    prison record type

--- malhar/memory_safety/debugging_safety.nim
+++ malhar/memory_safety/debugging_safety.nim
@@ -1,9 +1,16 @@
 type
     prisonReason* {.size:sizeof(uint8).} = enum
         # Reasons if a reference was invalid/dead, out of sync with stack. 
-        
+
         PreemptiveDeallocation     # user forcing to release memory pre-emptively (aka without scope exit).
         ReleasedDuringAssignment   # var b = a, b = c, then b has to remove older reference first !
         MovedToThreadByUser  # moving to a different Thread by user!
         ReallocationHappenedInThisThread # realloc routine responsible.
         ReallocationHappenedInOtherThread # realloc routine responsible.
+
+type
+    PrisonRecord = object
+        record_pointer:pointer
+        reference_id:uint8
+        record_payload:Natural     # payload associated to distinguish record_pointers, as they could be reused!
+        reason:prisonReason
