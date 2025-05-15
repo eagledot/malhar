cc1b2bfb45122294ee7a5396c130737945c886d9
eagledot <usernainn@zohomail.in>
2025-05-15 12:43:39 +0530

    a block of allocator

--- malhar/memory_safety/areana.nim
+++ malhar/memory_safety/areana.nim
@@ -8,3 +8,10 @@ include ./memory_safety
 # record-payload
 var 
     record_counter = 1  # would be accessed under a lock (This will act a payload for book-keeping and user stack-data )
+
+type
+    Block = object
+        n_records:Natural
+        capacity:Natural 
+        size:Natural  # current size (< capacity) for this block.
+        memory:pointer
