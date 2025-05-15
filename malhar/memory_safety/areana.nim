0679a70563263c5b1248102ab518b4a8c840ef2a
eagledot <usernainn@zohomail.in>
2025-05-15 12:44:29 +0530

    allocator type

--- malhar/memory_safety/areana.nim
+++ malhar/memory_safety/areana.nim
@@ -17,3 +17,10 @@ type
         memory:pointer
 proc `=copy`(a:var Block, b:Block){.error.}
 proc `=sink`(a:var Block, b:Block){.error.}
+
+type
+    ArenaAllocator = object
+        # lock:Lock              # for all operations, making it thread-safe, yes contention would be there.. but if user is careful about allocations then better than malloc atleast?
+        blocks:array[16, Block] # [1mb, 2mb, 4mb, ...]
+        # book-keeping (optional/modular, to (try to) prevent memory-safety bugs!)
+        bookkeeper*:BookKeeping
