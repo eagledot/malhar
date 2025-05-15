e7614224d23f25964839944922384cdbd1a1b397
eagledot <usernainn@zohomail.in>
2025-05-15 12:43:06 +0530

    record payload

--- malhar/memory_safety/areana.nim
+++ malhar/memory_safety/areana.nim
@@ -4,3 +4,7 @@ import system/ansi_c  # c_malloc, c_free!
 import math
 
 include ./memory_safety
+
+# record-payload
+var 
+    record_counter = 1  # would be accessed under a lock (This will act a payload for book-keeping and user stack-data )
