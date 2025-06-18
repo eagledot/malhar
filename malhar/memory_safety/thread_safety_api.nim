be92b139791cd0da926b5dd99ed9662a84fca048
eagledot <usernainn@zohomail.in>
2025-06-18 12:57:28 +0530

    types declaration

--- malhar/memory_safety/thread_safety_api.nim
+++ malhar/memory_safety/thread_safety_api.nim
@@ -2,3 +2,10 @@
 # For now: (try to) prove that `no concurrent access` to a shared byte is being done from different threads!
 import system/ansi_c
 import ./bit_array_simple
+
+type
+    ThreadMetaData* = object
+        readMismatchArray:BitArray
+        writeArray:BitArray
+        threadIds*:ptr UncheckedArray[int] # corresponding thread-id which will do a read/write. 
+proc `=copy`(a:var ThreadMetaData, b:ThreadMetaData){.error.}
