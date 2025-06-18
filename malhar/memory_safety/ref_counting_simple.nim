cc35ef04900bc783b65b3674bbdf713aa6fcdd0b
eagledot <usernainn@zohomail.in>
2025-06-18 16:37:33 +0530

    check the presence of a record

--- malhar/memory_safety/ref_counting_simple.nim
+++ malhar/memory_safety/ref_counting_simple.nim
@@ -13,3 +13,14 @@ type
 const MAX_LIVE_RECORDS_COUNT = 1024 # at one time it can support this number of allocations/records!
 var ref_count_arr*:array[MAX_LIVE_RECORDS_COUNT, RefCount] # put it on the stack.
 var record_counter:RecordPayload = 1
+
+proc has_record*(record_pointer:pointer, record_payload:RecordPayload):tuple[flag:bool, record_idx:int] =
+    doAssert not isNil(record_pointer), "not expected, right!"
+    var found_ix:int = -1
+    for i in 0..<MAX_LIVE_RECORDS_COUNT:
+        # find the corresponding slot
+        if ref_count_arr[i].record_pointer == record_pointer and record_payload == ref_count_arr[i].record_payload:
+            found_ix = i
+            break
+    result = (found_ix >= 0, found_ix)
+    return result
