309392933885ebb45170bd3ce891c68098ce5d79
eagledot <usernainn@zohomail.in>
2025-06-18 12:41:00 +0530

    types declaration

--- malhar/memory_safety/bit_array_simple.nim
+++ malhar/memory_safety/bit_array_simple.nim
@@ -1,3 +1,12 @@
 # Bit array implementation. (being used to check/uncheck the presence of an index).
 
 import system/ansi_c   # c_malloc, c_free 
+
+type
+    BitArrayScalar = uint # or int
+
+type
+    BitArray* = object
+        n_indices*:Natural # 1 bit for each index.
+        capacity_in_bytes:Natural  # in bytes. (given n_indices.. we calculate capacity in bytes)
+        data:ptr UncheckedArray[BitArrayScalar]
